f0da329801be138b33918e6a705108b8fadf4f5a
Michel Barbour Salvador <michel.barbou@wallbox.com>
2020-06-24 20:27:03 +0200

    Adds base multiplatform project

---
+++ MoviesMultiplatform/Shared/MoviesMultiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesMultiplatformApp.swift
+//  Shared
+//
+//  Created by Michel Barbou Salvador on 24/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct MoviesMultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
